10ebb8a8af565945eeefc06036a6cf383bccb06d
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-02-05 14:26:32 +0100

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" type-annotated function to return the floor of the float. """
+
+
+def floor(n: float) -> int:
+    """ return the floor of the float """
+    return (int(n))
