0626d47e2d5d402b94cc096363c262a4f35ec140
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-02-14 09:43:54 +0100

    pep8

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -9,11 +9,11 @@ class BasicCache(BaseCaching):
     """ basic caching system """
     def put(self, key, item):
         """ put element to the dict """
-        if(key != None and item != None):
+        if key != None and item != None:
             self.cache_data[key] = item
 
     def get(self, key):
         """ get element in the dict """
-        if(key == None or key not in self.cache_data):
+        if key == None or key not in self.cache_data:
             return None
         return self.cache_data[key]
